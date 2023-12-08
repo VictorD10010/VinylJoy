dca9ce67c1bee0eef20d7dd46443d768ca1626c5
VictorD10010 <81129601+VictorD10010@users.noreply.github.com>
2023-12-07 21:52:10 -0300

    Add files via upload

---
+++ src/app/firestore/firestore.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { firebaseConfig } from 'src/firebase.config';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule, 
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ]
+})
+export class FirestoreModule { }
