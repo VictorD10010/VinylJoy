e014f0ecf793b226e76296530585da397590d680
VictorD10010 <81129601+VictorD10010@users.noreply.github.com>
2023-12-21 04:46:00 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,11 @@ import { Disco } from './shared/model/disco';
 })
 export class AppComponent {
   title = 'Vinyljoy';
-  /**disco: Disco;
-  estante: Array<Disco>;
-  discoExclude: Disco;*/
+
 
   constructor(){
-    /*this.disco = new Disco();
-    this.estante = new Array<Disco>();
-    this.discoExclude = new Disco();*/
+   
   }
-
-  /*addDisco(): void {
-    this.estante.push(this.disco);
-    this.disco = new Disco();
-  }
-  rmDisco(): void {
-    let discRemove = this.estante.findIndex(disco => disco.barCode === this.discoExclude.barCode);
-    this.estante.splice(discRemove, 1);
-  }*/
 
 
 }
